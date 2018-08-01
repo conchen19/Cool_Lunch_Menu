3ef1fc6103118153e8892dcb56ef0d4913ee32f2
conchen19 <conniechen04@gmail.com>
2018-08-01 15:54:59 -0400

    first commit

---
+++ lunch_Menu.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var sand1 = document.getElementById('mondayImage')
+    console.log(sand1)
+    sand1.addEventListener('click', function (e) {
+    //    document.getElementById('mondayImage').src = "mondayimage2.jpg"
+    
+    })
+} )
